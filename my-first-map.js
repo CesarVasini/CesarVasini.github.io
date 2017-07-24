1e7916c568b1b107839b119084d400ea5d96b9f8
CesarVasini <cvasini@bol.com.br>
2017-07-23 21:35:51 -0300

    Delete my-first-map.js

--- my-first-map.js
+++
@@ -1,6 +0,0 @@
-function init(){
-map = new OpenLayers.Map('draw-map-here');
-originalOSM = new OpenLayers.Layer.OSM("OpenStreetMap");
-map.addLayers([originalOSM]);
-map.setCenter(new OpenLayers.LonLat(96.33899700000126,2.4700000000000246).transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject()), 10);
-}
